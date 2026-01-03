06cf2448f9a4d76cbe0dd3db5f03fa202d290ac4
Heitor <mafaldoheitor@gmail.com>
2026-01-03 19:14:01 -0300

    Adicionando mais mensagens para o tópico de janeiro e adicionando o estilo 'dark' para a minha imagem

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn_header = document.getElementById("btn_header")
 const title_section = document.querySelectorAll("[data-section-title]")
 const text_section = document.querySelectorAll("[data-section-text]")
 const section_main = document.querySelectorAll("[data-section-main]")
+const imageHeitor = document.querySelector("[data-section-image]")
 
 btn_header.addEventListener('click',()=>{
     body.classList.toggle("body-white")
@@ -18,4 +19,6 @@ btn_header.addEventListener('click',()=>{
     text_section.forEach(text => {
         text.classList.toggle("section__text_dark")
     })
+
+    imageHeitor.classList.toggle("section__img-dark")
 })
