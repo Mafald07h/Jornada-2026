cb4dcba992a7cb8813082660d0fcf0071d4dfda4
Heitor <mafaldoheitor@gmail.com>
2026-01-03 09:12:10 -0300

    Fazendo os elementos mudarem seus estilos de acordo com o tema

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const body = document.getElementById("body")
 const btn_header = document.getElementById("btn_header")
+const title_section = document.querySelector("[data-section-title]")
+const text_section = document.querySelector("[data-section-text]")
+const section_main = document.querySelector("[data-section-main]")
 
 btn_header.addEventListener('click',()=>{
     body.classList.toggle("body-white")
+    title_section.classList.toggle("section_title_dark")
+    text_section.classList.toggle("section__text_dark")
+    section_main.classList.toggle("section_dark")
 })
