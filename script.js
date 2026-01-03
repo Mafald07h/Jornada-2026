70a3c7554733998edd41015f12d6de9a2994f924
Heitor <mafaldoheitor@gmail.com>
2026-01-03 09:31:53 -0300

    Melhorando o código das seções e adicionando mais funcionalidades ao Javascript

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const body = document.getElementById("body")
 const btn_header = document.getElementById("btn_header")
-const title_section = document.querySelector("[data-section-title]")
-const text_section = document.querySelector("[data-section-text]")
-const section_main = document.querySelector("[data-section-main]")
+const title_section = document.querySelectorAll("[data-section-title]")
+const text_section = document.querySelectorAll("[data-section-text]")
+const section_main = document.querySelectorAll("[data-section-main]")
 
 btn_header.addEventListener('click',()=>{
     body.classList.toggle("body-white")
-    title_section.classList.toggle("section_title_dark")
-    text_section.classList.toggle("section__text_dark")
-    section_main.classList.toggle("section_dark")
+
+    section_main.forEach(section => {
+        section.classList.toggle("section_dark")
+    })
+
+    title_section.forEach(title => {
+        title.classList.toggle("section_title_dark")
+    })
+    
+    text_section.forEach(text => {
+        text.classList.toggle("section__text_dark")
+    })
 })
